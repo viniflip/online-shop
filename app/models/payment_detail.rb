6149a4d3ee1ccf835faaf22f63ad15b791087bb9
vinicius <ti@nxt1.com.br>
2020-04-06 00:01:49 +0100

    payment_detail validation

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,6 +1,9 @@
 class PaymentDetail < ApplicationRecord
   belongs_to :order
 
+  validates :account_number, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 100000, message: 'must be smaller than 100000' }
+  validates :issuing_bank, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10000000000, message: 'must be smaller than 10000000000' }
+
   def payment_information
     "Account Number: #{account_number} - Issuing Bank: #{issuing_bank}"
   end
