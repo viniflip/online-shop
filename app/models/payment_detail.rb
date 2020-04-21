b7a76a568e87d33626051e6d7d47328dda7d80ab
vinicius <ti@nxt1.com.br>
2020-04-21 18:54:20 +0100

    hotfix payments details validation

--- app/models/payment_detail.rb
+++ app/models/payment_detail.rb
@@ -1,8 +1,8 @@
 class PaymentDetail < ApplicationRecord
   belongs_to :order
 
-  validates :account_number, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 100000, message: 'must be smaller than 100000' }
-  validates :issuing_bank, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10000000000, message: 'must be smaller than 10000000000' }
+  validates :account_number, presence: true, numericality: { greater_than_or_equal_to: 1000000000, less_than_or_equal_to: 9999999999, message: 'must be between 1000000000 - 9999999999'}
+  validates :issuing_bank, presence: true, numericality: { greater_than_or_equal_to: 10000, less_than: 99999, message: 'must be between 10000 - 99999' }
 
   def payment_information
     "Account Number: #{account_number} - Issuing Bank: #{issuing_bank}"
